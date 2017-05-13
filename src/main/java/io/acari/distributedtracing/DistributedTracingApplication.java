9839b74bd617b34cb5c4310c63375e467eac2630
cyclic-reference <alex@cyclicreference.space>
2017-05-12 19:41:27 -0500

    Added rest controller, dunno what for

--- src/main/java/io/acari/distributedtracing/DistributedTracingApplication.java
+++ src/main/java/io/acari/distributedtracing/DistributedTracingApplication.java
@@ -2,11 +2,19 @@ package io.acari.distributedtracing;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class DistributedTracingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DistributedTracingApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String get(){
+		return "go away. \n";
 	}
 }
