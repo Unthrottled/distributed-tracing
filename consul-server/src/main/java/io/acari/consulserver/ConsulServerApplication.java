d4467934419908655c26a17fea4f7e9be51ce399
cyclic-reference <alex@cyclicreference.space>
2017-05-15 11:53:38 -0500

    More Modules

---
+++ consul-server/src/main/java/io/acari/consulserver/ConsulServerApplication.java
@@ -0,0 +1,12 @@
+package io.acari.consulserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsulServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsulServerApplication.class, args);
+	}
+}
