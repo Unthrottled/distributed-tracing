3f6fb6906b47eaaab3b0c8e2f7909de85e8b06d2
cyclic-reference <alex@cyclicreference.space>
2017-05-15 12:52:00 -0500

    Consul needs a cluster, can make eureka server though

--- eureka-server/src/test/java/io/acari/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/io/acari/eurekaserver/EurekaServerApplicationTests.java
@@ -1,4 +1,4 @@
-package io.acari.consulserver;
+package io.acari.eurekaserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ConsulServerApplicationTests {
+public class EurekaServerApplicationTests {
 
 	@Test
 	public void contextLoads() {
