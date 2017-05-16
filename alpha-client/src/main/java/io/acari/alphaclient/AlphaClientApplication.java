12798182c02b46e21fcdb94945552887b927854b
cyclic-reference <alex@cyclicreference.space>
2017-05-16 15:09:50 -0500

    I am on the right track

--- alpha-client/src/main/java/io/acari/alphaclient/AlphaClientApplication.java
+++ alpha-client/src/main/java/io/acari/alphaclient/AlphaClientApplication.java
@@ -1,22 +1,28 @@
 package io.acari.alphaclient;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
 import org.springframework.http.ResponseEntity;
+import org.springframework.integration.annotation.MessageEndpoint;
+import org.springframework.integration.annotation.ServiceActivator;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import javax.ws.rs.core.MediaType;
 
 @EnableDiscoveryClient
 @EnableFeignClients
 @SpringBootApplication
+@EnableBinding(Sink.class)
 public class AlphaClientApplication {
 	public static final String SERVICE_NAME = "service-alpha";
 
@@ -25,8 +31,18 @@ public class AlphaClientApplication {
 	}
 }
 
+@MessageEndpoint
+class MessageSink {
+	Log log = LogFactory.getLog(MessageSink.class);
+
+	@ServiceActivator(inputChannel = Sink.INPUT)
+	public void acceptMessage(String message){
+		this.log.info("Got message: " + message);
+	}
+}
+
 @FeignClient(serviceId = AlphaClientApplication.SERVICE_NAME)
-interface RestReader {
+interface RestClient {
 	@RequestMapping(method = RequestMethod.GET, value = "/", consumes = MediaType.APPLICATION_JSON)
 	String getMessageYo();
 }
@@ -34,16 +50,16 @@ interface RestReader {
 @RestController
 @RequestMapping("/message")
 class ClientRestController {
-	private RestReader restReader;
+	private RestClient restClient;
 
 	@Autowired
-	public ClientRestController(RestReader restReader) {
-		this.restReader = restReader;
+	public ClientRestController(RestClient restClient) {
+		this.restClient = restClient;
 	}
 
 	@RequestMapping("/client")
 	ResponseEntity<String> callService(){
-		return ResponseEntity.ok(restReader.getMessageYo());
+		return ResponseEntity.ok(restClient.getMessageYo());
 	}
 
 }
--- service-alpha/src/main/java/io/acari/distributedtracing/DistributedTracingApplication.java
+++ service-alpha/src/main/java/io/acari/distributedtracing/DistributedTracingApplication.java
@@ -1,41 +1,56 @@
 package io.acari.distributedtracing;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
+import org.springframework.integration.annotation.Gateway;
+import org.springframework.integration.annotation.MessagingGateway;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
 import java.util.List;
-import java.util.Properties;
+
+@MessagingGateway
+interface MessagingSource {
+    @Gateway(requestChannel = Source.OUTPUT)
+    void sendMessage(String message);
+}
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableBinding(Source.class)
 public class DistributedTracingApplication {
+    private static final Logger log = LoggerFactory.getLogger(DistributedTracingApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(DistributedTracingApplication.class, args);
-	}
-
-
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedTracingApplication.class, args);
+    }
 }
 
 @RestController
 class RestGuy {
 
-	private DiscoveryClient discoStu;
+    private DiscoveryClient discoStu;
+    private MessagingSource messagingSource;
 
-	@Autowired
-	public RestGuy(DiscoveryClient discoStu) {
-		this.discoStu = discoStu;
-	}
+    @Autowired
+    public RestGuy(DiscoveryClient discoStu, MessagingSource messagingSource) {
+        this.discoStu = discoStu;
+        this.messagingSource = messagingSource;
+    }
 
-	@RequestMapping("/")
-	public List<String> get(){
-		List<String> services = discoStu.getServices();
-		services.add("Piss Off.");
-		return services;
-	}
+    @RequestMapping("/")
+    public List<String> get() {
+        List<String> services = discoStu.getServices();
+        services.add("Piss Off.");
+        messagingSource.sendMessage("Sending " + services + " @ " + Instant.now());
+        return services;
+    }
 }
