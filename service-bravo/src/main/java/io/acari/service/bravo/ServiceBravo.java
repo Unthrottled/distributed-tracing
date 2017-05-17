5efcdd5927378b9b0d0cd77e69f90eab7ae954a5
cyclic-reference <alex@cyclicreference.space>
2017-05-17 14:02:18 -0500

    Final Piece of the puzzlegit add .

--- service-bravo/src/main/java/io/acari/service/bravo/ServiceBravo.java
+++ service-bravo/src/main/java/io/acari/service/bravo/ServiceBravo.java
@@ -3,9 +3,12 @@ package io.acari.service.bravo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableBinding(Source.class)
 public class ServiceBravo {
 
     public static void main(String[] args) {
--- service-charlie/src/main/java/io/acari/service/charlie/ServiceCharlie.java
+++ service-charlie/src/main/java/io/acari/service/charlie/ServiceCharlie.java
@@ -3,9 +3,15 @@ package io.acari.service.charlie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+import org.springframework.cloud.stream.messaging.Source;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableFeignClients
+@EnableBinding({Sink.class, Source.class})
 public class ServiceCharlie {
 
     public static final String ZULU_NAME = "service-zulu";
