eee4cfc832b95e03b4760c131494e34acd0b5c25
cyclic-reference <alex@cyclicreference.space>
2017-05-17 08:35:28 -0500

    Less rest controllers for now

--- service-bravo/src/main/java/io/acari/alpha/ServiceBravo.java
+++ service-bravo/src/main/java/io/acari/alpha/ServiceBravo.java
@@ -1,38 +1,14 @@
 package io.acari.alpha;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ServiceBravo {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceBravo.class, args);
-	}
-
-
-}
-
-@RestController
-class RestGuy {
-
-	private DiscoveryClient discoStu;
-
-	@Autowired
-	public RestGuy(DiscoveryClient discoStu) {
-		this.discoStu = discoStu;
-	}
-
-	@RequestMapping("/")
-	public List<String> get(){
-		return discoStu.getServices();
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceBravo.class, args);
+    }
 }
--- service-charlie/src/main/java/io/acari/alpha/ServiceCharlie.java
+++ service-charlie/src/main/java/io/acari/alpha/ServiceCharlie.java
@@ -1,38 +1,15 @@
 package io.acari.alpha;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 public class ServiceCharlie {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ServiceCharlie.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceCharlie.class, args);
+    }
 
-
-}
-
-@RestController
-class RestGuy {
-
-	private DiscoveryClient discoStu;
-
-	@Autowired
-	public RestGuy(DiscoveryClient discoStu) {
-		this.discoStu = discoStu;
-	}
-
-	@RequestMapping("/")
-	public List<String> get(){
-		return discoStu.getServices();
-	}
 }
