b6fcbb52aa1a53f1eb7c5b3d91e2d4b2acb3ccad
cyclic-reference <alexsimons9999@gmail.com>
2017-12-05 06:59:54 -0600

    parking

--- alpha-client/web-content/src/app/app.component.ts
+++ alpha-client/web-content/src/app/app.component.ts
@@ -1,14 +1,26 @@
-import {Component, OnInit} from "@angular/core";
+import {Component} from "@angular/core";
 import "./app.component.htm";
 import {Message} from "./Message.model";
+import {BackendService} from "./Backend.service";
 
 @Component({
     selector: 'angular-application',
     template: require('./app.component.htm')
 })
-export class AppComponent implements OnInit {
-    ngOnInit(): void {
+export class AppComponent {
+    private messages: Message[] = [new Message('ayy lmao')];
+
+
+    constructor(private backendService: BackendService) {
+
     }
 
-    private messages: Message[] = [];
+    public fetchMessage(): void {
+        this.backendService.fetchMessage()
+            .subscribe(oneMessage=> this.messages.push(oneMessage));
+    }
+
+    public clearMessages(): void {
+        this.messages = [];
+    }
 }
