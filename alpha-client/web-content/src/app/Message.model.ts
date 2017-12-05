3bc70337ab501eb367197147c6bd2a2fa4aedbc5
cyclic-reference <alex@cyclicreference.space>
2017-12-05 15:40:33 -0600

    more visual

--- alpha-client/web-content/src/app/Message.model.ts
+++ alpha-client/web-content/src/app/Message.model.ts
@@ -4,4 +4,12 @@ export class Message {
     constructor(public value: string){
 
     }
+
+    get succeeded(): boolean {
+        return this.value.indexOf("Try again") < 0;
+    }
+
+    get failed(): boolean {
+        return !this.succeeded;
+    }
 }
